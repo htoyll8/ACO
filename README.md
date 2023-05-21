33455169f84af55218c27776153f2052dc8b30ed
Tyler Holloway <tilooor@Tylers-MacBook-Pro-3.local>
2023-05-21 09:16:41 -0400

    Implemented choose_path and updated READ.me

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# ACO
+# Ant Colony Optimization
+
+This repository contains a computer program that implements the Ant Colony Optimization (ACO) algorithm and includes variations of the algorithm for different problem domains. The implementation is based on the concepts described in the book "ACO" by Marco Dorigo and Thomas Stutzle.
+
+## Introduction
+
+Ant Colony Optimization is a metaheuristic algorithm inspired by the foraging behavior of ants. It utilizes the concept of pheromone trails to find optimal solutions for various optimization problems. This repository provides an implementation of the ACO algorithm and its variations.
+
+## Variations
+
+This repository includes the following variations of the ACO algorithm:
+
+### Double Bridge Experiment
+
+The double bridge experiment is a classic example used to study the optimization capabilities of ant colonies. It demonstrates how ants can find the shortest path between two points by using probabilistic rules based on local information. The implementation for this variation can be found in the [double_bridge](double_bridge) subfolder.
+
+### [Add Your Variation]
+
+Feel free to add your own variations of the ACO algorithm by creating a new subfolder and implementing the necessary code.
+
+## Usage
+
+Each variation of the ACO algorithm has its own implementation and usage instructions. Refer to the respective subfolders for detailed information on running and customizing the algorithms.
+
+## References
+
+- Dorigo, M., & Stutzle, T. (Year). ACO. Publisher.
--- double_bridge/double_bridge_aco.py
+++ double_bridge/double_bridge_aco.py
@@ -1,20 +1,35 @@
+import random
+
 class Ant:
     def __init__(self, node, speed=1):
         self.node = node
         self.speed = speed
 
-    def choose_path(self):
-        pass
+    def choose_path(self, path_short, path_long, alpha):
+        total_pheromone = (path_short.pheromone ** alpha) + (path_long.pheromone ** alpha)
+        if total_pheromone == 0:
+            return random.choice([path_short, path_long])
+        
+        p_short = (path_short.pheromone ** alpha) / total_pheromone
+        if random.random() < p_short:
+            return path_short
+        else:
+            return path_long
 
 class Path:
     def __init__(self, length, pheromone=0):
         self.length = length
         self.pheromone = pheromone
 
-    def update_pheromone(self):
+    def update_pheromone(self, p, m):
+        # ρ_is(t-1) = ρ_is(t-1) + p_is(t-1)m_i(t-1) + p_js(t-1)m_j(t-1), (i=1, j=2; i=2, j=1)
+        # OR
+        # ρ_il(t) = ρ_il(t-1) + p_il(t-1)m_i(t-1) + p_jl(t-1)m_j(t-r), (i=1, j=2; i=2, j=1)
+        # WHERE
+        # m_i(t) = p_js(t-1)m_j(t-1) + p_jl(t-r)m_j(t-r)
         pass
 
-class Graph: 
+class Graph:
     def __init__(self):
         self.paths = {}
 
@@ -24,9 +39,10 @@ class Graph:
     def get_path(self, path_name):
         return self.paths[path_name]
     
-    def update_path(self, path_name):
-        self.paths[path_name].update_pheromone()
+    def update_path(self, path_name, p, m):
+        self.paths[path_name].update_pheromone(p, m)
 
 class Node:
-    def __init__(self, ant_count):
+    def __init__(self, id, ant_count):
+        self.id = id
         self.ant_count = ant_count
