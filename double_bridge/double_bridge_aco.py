d290424d535a4a3069b489bf724e1fa9b85f24a5
Tyler Holloway <tilooor@Tylers-MacBook-Pro-3.local>
2023-05-21 15:20:10 -0400

    Added paths from nodes. I still don't understand update_pheremones.

--- double_bridge/double_bridge_aco.py
+++ double_bridge/double_bridge_aco.py
@@ -6,6 +6,7 @@ class Ant:
         self.speed = speed
 
     def choose_path(self, path_short, path_long, alpha):
+        # Need to consider the node the ant is currently at. This isn't quite right yet.
         total_pheromone = (path_short.pheromone ** alpha) + (path_long.pheromone ** alpha)
         if total_pheromone == 0:
             return random.choice([path_short, path_long])
@@ -21,7 +22,8 @@ class Path:
         self.length = length
         self.pheromone = pheromone
 
-    def update_pheromone(self, p, m):
+    def update_pheromone(self, t):
+        self.pheromone += rho_is(t-1) + p_is(t-1) * m_i(t-1) + p_js(t-1) * m_j(t-1)
         # ρ_is(t-1) = ρ_is(t-1) + p_is(t-1)m_i(t-1) + p_js(t-1)m_j(t-1), (i=1, j=2; i=2, j=1)
         # OR
         # ρ_il(t) = ρ_il(t-1) + p_il(t-1)m_i(t-1) + p_jl(t-1)m_j(t-r), (i=1, j=2; i=2, j=1)
@@ -29,20 +31,35 @@ class Path:
         # m_i(t) = p_js(t-1)m_j(t-1) + p_jl(t-r)m_j(t-r)
         pass
 
-class Graph:
-    def __init__(self):
-        self.paths = {}
-
-    def add_path(self, path_name, path_length):
-        self.paths[path_name] = Path(length=path_length)
-
-    def get_path(self, path_name):
-        return self.paths[path_name]
-    
-    def update_path(self, path_name, p, m):
-        self.paths[path_name].update_pheromone(p, m)
-
 class Node:
     def __init__(self, id, ant_count):
         self.id = id
         self.ant_count = ant_count
+        self.paths = {}
+
+    def add_path(self, path_name, path):
+        self.paths[path_name] = path
+
+    def get_path(self, path_name):
+        return self.paths[path_name]
+
+    def update_path(self): 
+        for path in self.paths:
+            path.update_pheromone()
+
+if __name__ == "__main__":
+    # Create the graph
+    node1 = Node(id=1, ant_count=10)
+    node2 = Node(id=2, ant_count=10)
+
+    path_short = Path(length=10)
+    path_long = Path(length=20)
+
+    node1.add_path("short", path_short)
+    node1.add_path("long", path_long)
+    node2.add_path("short", path_short)
+    node2.add_path("long", path_long)
+
+    # Set initial pheromone levels
+    path_short.pheromone = 0.1
+    path_long.pheromone = 0.1
